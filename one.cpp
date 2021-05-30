2f4aae4757d76faecb330cde68053f0641be037c
Tushar Saxena <stushar345@gmail.com>
2021-05-30 23:38:14 +0530

    Update one.cpp

--- one.cpp
+++ one.cpp
@@ -1,13 +1,11 @@
-#include<bits/stdc++.h>
-using namespace std;
-   vector<int> nextPermutation(vector<int>& nums) 
+void nextPermutation(vector<int>& nums) 
     {
-        int index=-1;
+         int index=-1;
         int prev=-1;
         int n=nums.size();
-        for(int i=n-1;i>0;i--)
+        for(int i=n-2;i>=0;i--)
         {
-            if(nums[i]>nums[i-1])
+            if(nums[i+1]>nums[i])
             {
                 index=i;
                 break;
@@ -17,38 +15,16 @@ using namespace std;
             reverse(nums.begin(),nums.end());
         else
         {
-            for(int i=n-1;i>=index;i--)
+            for(int i=n-1;i>index;i--)
             {
-                if(nums[i]>nums[index-1])
+                if(nums[i]>nums[index])
                 {
                     prev=i;
 		    break;
                 }
             }
         
-        swap(nums[index-1],nums[prev]);
-        reverse(nums.begin()+index,nums.end());
+        swap(nums[index],nums[prev]);
+        reverse(nums.begin()+index+1,nums.end());
         }
-	return nums;
-    }  
-
-int main()
-{
-	int n;
-	cin>>n;
-
-	vector<int> v;
-	int a;
-	for(int i=0;i<n;i++)
-	{
-		cin>>a;
-		v.push_back(a);						//int arr[]={5,15,10,8,6,12,9,18};
-	}
-
-	v=nextPermutation(v);
-	for(auto itr:v)
-	{
-		cout<<itr<<" ";
-	}
-
-}
+    }
